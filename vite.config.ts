2f294b2489a9fbae967857cdb0b34099083b0592
Joshua Muhali <joshmuherly@gmail.com>
2025-07-14 19:50:25 +0200

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,29 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
-import { componentTagger } from 'lovable-tagger';
-import { fileURLToPath } from 'url';
+// import path from 'path';
+// import { componentTagger } from 'lovable-tagger';
+// import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-// Resolve path aliases
-const resolvePath = (p: string) => path.resolve(__dirname, p);
+// // Resolve path aliases
+// const resolvePath = (p: string) => path.resolve(__dirname, p);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': resolvePath('./src')
-    }
-  },
-  server: {
-    host: '::',
-    port: 8080
-  },
+  // resolve: {
+  //   alias: {
+  //     '@': resolvePath('./src')
+  //   }
+  // },
+  // server: {
+  //   host: '::',
+  //   port: 8080
+  // },
   plugins: [
     react(),
-    componentTagger()
+    // componentTagger()
   ],
   // base: "/platchemNew"
 });
